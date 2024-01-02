c8964b92787fc26a7782d8f273d451d0fcf58b49
natagetup <n.kulikouskaya@gmail.com>
2024-01-02 12:00:13 +0100

    feat: add getAverage task implementation

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -135,8 +135,14 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  const sum = arr.reduce((accum, item) => {
+    return accum + item;
+  }, 0);
+
+  const average = arr.length ? sum / arr.length : 0;
+
+  return Number.isInteger(average) ? average : +average.toFixed(2);
 }
 
 /**
